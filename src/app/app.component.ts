9c15389249c024fc9afcac879f91e7170b208731
Edwin van der Ven <edwinvanderven@gmail.com>
2019-05-02 21:57:15 +0200

    Added an onClick to reveal the answers for all except the latest so during startup the processor doesn't have to do the calculations for every problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PROBLEMS, IProblem } from './app.component.model';
 
 @Component({
@@ -9,4 +9,10 @@ import { PROBLEMS, IProblem } from './app.component.model';
 export class AppComponent {
   public title = 'euler-in-angular';
   public problems: IProblem[] = PROBLEMS;
+
+  public showAnswers: boolean = false;
+
+  public onHiddenTextClick() {
+    this.showAnswers = true;
+  }
 }
