2215d181a80291d5daf22a6be4877819c53028eb
Edwin van der Ven <edwinvanderven@gmail.com>
2019-09-15 11:49:44 +0200

    Added fortieth problem and solution

---
+++ src/app/problems/40-to-49/40.spec.ts
@@ -0,0 +1,17 @@
+import { P40 } from './40';
+
+describe('P40', () => {
+  let problem: P40;
+
+  beforeEach(() => {
+    problem = new P40();
+  });
+
+  it('should exist', () => {
+    expect(problem).toBeDefined();
+  });
+
+  it('should return the correct answer', () => {
+    expect(problem.getAnswer()).toEqual('210');
+  });
+});
